55c12293ae4d40eb80e9462161f5444912b2bb4d
Reményi Gergely <remenyig@gmail.com>
2024-02-10 20:03:15 +0100

    Remove unnecessary endpoints

--- frontend/app.py
+++ frontend/app.py
@@ -7,8 +7,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/", methods = ["GET"])
 def index():
-    #recipes = requests.get("http://backend:4200/recipes").json()
-    recipes = []
+    recipes = requests.get("http://localhost:4200/recipes").json()
 
     return render_template("recipe_list_page.html", recipes=recipes)
 
@@ -24,46 +23,5 @@ def add_recipe_step_2():
     tags.pop(0) # Removes the tag written in the text input at the time of the delete
     name = request.form.get("name")
 
-    return render_template('add_recipe_form_2.html', tags=tags, name=name, form_title="Please add the ingredients")
-
-
-@app.route("/add_tag", methods = ["POST"])
-def add_tag():
-    tags = request.form.getlist('tag')
-
-    return render_template('tags.html', tags=tags)
-
-
-@app.route("/delete_tag/<tag_name>", methods = ["DELETE"])
-def delete_tag(tag_name):
-    tags = request.form.getlist('tag')
-
-    tags.remove(tag_name)
-    tags.pop(0) # Removes the tag written in the text input at the time of the delete
-
-    return render_template('tags.html', tags=tags)
-
-
-@app.route("/add_ingredient", methods = ["POST"])
-def add_ingredient():
-    name = request.form.get("ingredient_name")
-    amount = request.form.get("amount")
-    measurement = request.form.get("measurement")
-
-    ingredient = f"{name} - {amount} {measurement}"
-
-    ingredients = request.form.getlist('ingredient')
-
-    if name != "" and amount != "":
-        ingredients.append(ingredient)
-
-    return render_template('ingredients.html', ingredients=ingredients, ingredient_name=name, amount=amount)
-
-
-@app.route("/delete_ingredient/<ingredient>", methods = ["DELETE"])
-def delete_ingredient(ingredient):
-    ingredients = request.form.getlist('ingredient')
-    ingredients.remove(ingredient)
-
-    return render_template('ingredients.html', ingredients=ingredients)
+    return render_template('add_recipe_form_2.html', form_title="Please add the ingredients")
 
