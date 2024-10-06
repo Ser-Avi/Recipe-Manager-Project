7fb2f21a372ac2ddc92ac2b2670e35312eeca3f0
Reményi Gergely <remenyig@gmail.com>
2024-10-06 14:48:39 +0200

    Refactor backend

--- api.py
+++ api.py
@@ -1,30 +1,42 @@
-from flask import Flask, request, redirect
-from data import *
+from flask import Flask, request, jsonify
+from data import RecipeRepository, Recipe
+import json
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET"])
+recipe_repository = RecipeRepository()
+
+
+@app.route("/", methods=["GET"])
 def index():
-    return "Hello" #TODO: figure out if this should lead to anything
+    return "Hello"  # TODO: figure out if this should lead to anything
+
 
 @app.route("/recipes")
 def recipes():
-    return jsonify([get_data(i) for i in recipe_collection.find()])
+    all_recipes = recipe_repository.get_all_recipes()
+    return jsonify(all_recipes)
+
 
 @app.route("/recipes/<recipe_id>")
-def recipeID(recipe_id):
-    return jsonify(get_recipe(str(recipe_id)))
+def recipeID(recipe_id: str):
+    recipe = recipe_repository.get_recipe(recipe_id)
 
-@app.route("/submit", methods = ["Get","POST"])
+    if not recipe:
+        return jsonify("Recipe not found"), 404
+
+    return jsonify(recipe)
+
+
+
+@app.route("/submit", methods=["Get", "POST"])
 def newRecipe():
-    try:
-        Recipe(**request.get_json())
-    except:
-        return "ERROR! Recipe couldn't be added. Please check format."
-    else:
-        add_recipe(request.get_json())
-        return "Recipe added!"
+    json_map = json.loads(request.get_json())
+    recipe = Recipe(id=None, **json_map)
+    recipe_repository.add_recipe(recipe)
+    return ""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+    del recipe_repository
--- data.py
+++ data.py
@@ -1,42 +1,9 @@
-from dataclasses import dataclass
-from flask import jsonify
+from dataclasses import dataclass, field
 from pymongo import MongoClient
 from bson import ObjectId
+from typing import List
 
-#Initializing vars
-client = MongoClient('mongodb', 27017)
-db = client['recipe_db']
 
-tags_collection = db['tags']
-recipe_collection = db['recipes']
-
-#Methods
-def get_data(data):
-    """
-    got this method/solution for mongoDB->json convertion from https://stackoverflow.com/a/63206156
-    :param data: a MongoDB document
-    :return: the document in a jsonify-able format
-    """
-    data['_id'] = str(data['_id'])
-    return data
-
-def get_recipe(id:str):
-    return get_data(recipe_collection.find_one(ObjectId(id)))
-
-def add_recipe(recipe_json):
-    recipe_collection.insert_one(recipe_json)
-    curr_id = get_data(recipe_collection.find_one(recipe_json)).get('_id')
-    for tag in recipe_json.get('tags'):
-        tag_adder(tag, curr_id)
-
-def tag_adder(tag:str, recipe_id):
-    curr = tags_collection.find_one({'tag':tag})
-    if curr is None:
-        tags_collection.insert_one({'tag': tag, 'ids': [recipe_id]})
-    else:
-        tags_collection.update_one({'tag':tag},{"$set":{"ids": curr.get('ids') + [recipe_id]}})
-
-#Classes
 @dataclass
 class Ingredient:
     """
@@ -46,31 +13,59 @@ class Ingredient:
     amount: int
     measurement: str
 
+
 @dataclass
 class Recipe:
     """
     The Recipe class holds all the information about a recipe.
     Has custom method for  turning into a dict.
     """
-    #idKey: int                  #IDs are currently simple integer indexes
+    # idKey: int                  #IDs are currently simple integer indexes
+    id: str | None
     name: str
-    tags: [str]
-    timeToMake: float
+    tags: List[str]
+    timeToMake: int
     servings: int
-    ingredients: []
-    steps: [str]
+    steps: List[str]
+    ingredients: List[Ingredient] = field(default_factory=list)
 
-    def dictify(self):
-        ingredientsDictList = []
-        for ingredient in self.ingredients:
-            ingredientsDictList.append(ingredient.__dict__)
+    def to_dict(self):
+        return {k: v for k, v in self.__dict__.items() if k != 'id'}
 
-        dictVersion = {
-            "name": self.name,
-            "tags": self.tags,
-            "timeToMake": self.timeToMake,
-            "servings": self.servings,
-            "ingredients": ingredientsDictList,
-            "steps": self.steps
-        }
-        return dictVersion
+
+class RecipeRepository:
+
+    def __init__(self) -> None:
+        self.client = MongoClient('localhost', 27017)
+        db = self.client['recipe_db']
+        self.tags_collection = db['tags']
+        self.recipe_collection = db['recipes']
+
+    def __del__(self) -> None:
+        self.client.close()
+
+    def get_recipe(self, id: str) -> Recipe | None:
+        recipe_map = self.recipe_collection.find_one({'_id': ObjectId(id)})
+        return Recipe(id=str(recipe_map.pop("_id")), **recipe_map) if recipe_map else None
+
+    def get_all_recipes(self) -> List[Recipe]:
+        recipe_maps = self.recipe_collection.find()
+        recipes = map(
+            lambda r: Recipe(id=str(r.pop('_id')), **r),
+            recipe_maps)
+
+        return list(recipes)
+
+    def add_recipe(self, recipe: Recipe) -> None:
+        recipe_map = recipe.to_dict()
+        recipe_id = self.recipe_collection.insert_one(recipe_map).inserted_id
+        # TODO: bulk update
+        for t in recipe.tags:
+            self.__create_or_update_tag(t, recipe_id)
+
+    def __create_or_update_tag(self, tag_value: str, recipe_id: str) -> None:
+        tag = self.tags_collection.find_one({'tag': tag_value}) or {'ids': []}
+        self.tags_collection.update_one(
+            {'tag': tag_value},
+            {"$set": {'tag': tag_value, "ids": tag['ids'] + [recipe_id]}},
+            upsert=True)
